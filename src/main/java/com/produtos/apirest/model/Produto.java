5cc2ecf2122a6e630ff158572f193ed7ff7f4418
guilherme seixas <guilhermesantoseixas@outlook.com>
2022-02-11 16:19:43 -0300

    create produto model and repository, update app properties

---
+++ src/main/java/com/produtos/apirest/model/Produto.java
@@ -0,0 +1,52 @@
+package com.produtos.apirest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Produto {
+	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String nome;
+
+	private BigDecimal quantidade;
+
+	private BigDecimal valor;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+}
